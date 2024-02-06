74686dcf3faf55709a59bd0016532a525aa79eaa
Joe <joe.mr3@gmail.com>
2024-02-05 18:55:37 -0800

    Add files via upload

---
+++ towers_of_hanoi.js
@@ -0,0 +1,31 @@
+
+/**
+ * @param {int32} n
+ * @return {list_list_int32}
+ * 
+ * TC = O(2^n)
+ */
+function tower_of_hanoi(n) {
+    // Write your code here.
+    let res = []
+
+    number_of_moves(n, 1, 2, 3, res);
+    return res;
+}
+
+let number_of_moves = (n, source, aux, dest, res) => {
+  // base case
+  if (n === 1) {
+    res.push([source, dest])
+    return;
+  }
+
+  number_of_moves(n-1, source, dest, aux, res);
+  res.push([source, dest]);
+  number_of_moves(n-1, aux, source, dest, res);
+
+  return res;
+};
+
+let discs = tower_of_hanoi(4);
+console.log(discs);
